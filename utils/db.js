ef49a21aeddc03e6601770d4f8d3e8b945e65870
Abdellah HSSINI <abdellahhssini@gmail.com>
2026-04-13 18:44:26 +0100

    db Commit

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
+        console.log(process.env.MONGO_URL);
+        
         await mongoose.connect(process.env.MONGO_URL, {
             dbName: "Ecommerce2026",
         });
